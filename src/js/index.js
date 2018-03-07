5e46a4d22dc3d64c8b429212b46ff6144c4a76b0
lucker-lv <luckerlv@163.com>
2018-03-07 20:06:05 +0800

    add file
    123

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+/**
+  项目JS主入口
+**/
+layui.define(['layer', 'form', 'element'], function (exports) {
+    var layer = layui.layer,
+        form = layui.form;
+
+    layer.msg('Hello World');
+
+    exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
+});
